35681def560953de321ba87ce01c7514d06800c3
Michael Provenzano <proven24@students.rowan.edu>
2023-12-07 16:51:51 -0500

    Upated CORS.

--- app/main.py
+++ app/main.py
@@ -14,8 +14,10 @@ origins = [
 ]
 app.add_middleware(
         CORSMiddleware,
-        allow_origins=origins,
+        allow_origins=["*"],
+        allow_credentials=True,
         allow_methods=["*"],
+        allow_headers=["*"],
 )
 
 app.include_router(auth3.router)
