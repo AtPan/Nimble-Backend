a0f016e5d49ab22968e96890dd3692c9e0bb6f1c
Michael Provenzano <proven24@students.rowan.edu>
2023-12-09 20:20:29 -0500

    Bug fixes for user authentication.  Added SQL folder for building database containers.  Adjust yaml to build database containers with volumes for data persistance.

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,14 +1,21 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from JWTtoken import verify_token
+from user import fetch_user_by_email
+from db import get_db
+from sqlalchemy.orm import Session
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
     
-    return verify_token(token, credentials_exception)
+    username = verify_token(token, credentials_exception)
+    user = fetch_user_by_email(db, username)
+    if user is None:
+        raise credentials_exception
+    return user
